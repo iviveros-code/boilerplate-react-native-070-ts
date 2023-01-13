c8141701e427f6241fab802e4a07ebfb746469b3
Ivan Viveros <ivanviveros@MacBook-Pro-de-Ivan-4.local>
2023-01-13 09:56:29 -0300

    babel ts config

--- template/babel.config.js
+++ template/babel.config.js
@@ -7,20 +7,19 @@ module.exports = {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          "@assets": "./assets",
-          "@constants": "./src/constants",
-          "@components": "./src/components",
-          "@screens": "./src/screens",
-          "@config": "./src/config",
-          "@utils": "./src/utils",
-          "@theme": "./src/theme",
-          "@services": "./src/services",
-          "@navigation": "./src/navigation",
-          "@hooks": "./src/hooks",
-          "@redux": "./src/redux"
-        }
-      }
-    ]
-  ]
+          '@assets': './assets',
+          '@constants': './src/constants',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@config': './src/config',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+          '@hooks': './src/hooks',
+          '@redux': './src/redux',
+        },
+      },
+    ],
+  ],
 };
-
